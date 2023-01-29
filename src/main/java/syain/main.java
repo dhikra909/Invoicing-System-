c0d9040c458c189c0c6b2a058b754167b850a4da
dhikra909 <alm@ka909@gmail.com>
2023-01-29 00:27:52 -0800

    menu

--- src/main/java/syain/main.java
+++ src/main/java/syain/main.java
@@ -4,17 +4,60 @@ import java.util.Scanner;
 
 public class main {
 
-	public static void ShopSettings(String[] args) throws Throwable {
+	public static void main(String[] args) throws Throwable {
 
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Select one of the options:");
-		System.out.println("1. Load Data ");
-		System.out.println("2. Set Shop Name");
-		System.out.println("3. Set Invoice Header");
-		System.out.println("4. Go Back");
+		System.out.println("1. Shop Settings  ");
+		System.out.println("2. Manage Shop Items");
+		System.out.println("3. Create New Invoice");
+		System.out.println("4. Report: Statistics");
+		System.out.println("5. Report: All Invoices");
+		System.out.println("6. Search (1) Invoice");
+		System.out.println("7. Program Statistics");
+		System.out.println("8. Exit  ");
+
 		
+		int select = sc.nextInt();
+		
+		switch(select) {
+		case 1:
+			
+			break;
+		case 2:
+		
+			break;
+		case 3:	
+		
+			break;
+		case 4:	
 		
 			
+			break;
+		
+        case 5:	
+		
+			
+			break;
+        case 6:	
+		
+			
+			break;
+        case 7:	
+		
+		
+		break;
+        case 8:	
+        	System.out.println("Exit");
+		
+		break;
+		
+		
+		}
+		
+		
+		
+		
 	}
 	
 	
