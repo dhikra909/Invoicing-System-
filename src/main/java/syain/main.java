188246059ddaa49da9df8d1351a11b33e4084cbe
dhikra909 <alm@ka909@gmail.com>
2023-01-28 23:23:49 -0800

    main

---
+++ src/main/java/syain/main.java
@@ -0,0 +1,22 @@
+package syain;
+
+import java.util.Scanner;
+
+public class main {
+
+	public static void ShopSettings(String[] args) throws Throwable {
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Select one of the options:");
+		System.out.println("1. Load Data ");
+		System.out.println("2. Set Shop Name");
+		System.out.println("3. Set Invoice Header");
+		System.out.println("4. Go Back");
+		
+		
+			
+	}
+	
+	
+	
+}
